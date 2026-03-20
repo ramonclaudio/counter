9524f9659b98271f955f44043561d1e7f25595eb
Ray <hello@ramonclaudio.com>
2026-03-20 15:32:03 -0400

    fix: add ProvisioningStyle=Automatic to target attributes for device builds

--- plugins/with-auto-signing.js
+++ plugins/with-auto-signing.js
@@ -12,23 +12,34 @@ const withAutoSigning = (config) => {
       return config;
     }
 
+    // Set CODE_SIGN_STYLE and DEVELOPMENT_TEAM on build configurations
     const buildConfigurations = xcodeProject.pbxXCBuildConfigurationSection();
-
     for (const key in buildConfigurations) {
       const buildConfig = buildConfigurations[key];
-
       if (
         buildConfig.buildSettings &&
         buildConfig.buildSettings.PRODUCT_BUNDLE_IDENTIFIER
       ) {
         buildConfig.buildSettings.CODE_SIGN_STYLE = 'Automatic';
-
         if (config.ios?.appleTeamId) {
           buildConfig.buildSettings.DEVELOPMENT_TEAM = config.ios.appleTeamId;
         }
-
         delete buildConfig.buildSettings.PROVISIONING_PROFILE;
         delete buildConfig.buildSettings.PROVISIONING_PROFILE_SPECIFIER;
+      }
+    }
+
+    // Set ProvisioningStyle in project-level TargetAttributes
+    const projectSection = xcodeProject.pbxProjectSection();
+    for (const key in projectSection) {
+      const project = projectSection[key];
+      if (project.attributes && project.attributes.TargetAttributes) {
+        for (const targetId in project.attributes.TargetAttributes) {
+          project.attributes.TargetAttributes[targetId].ProvisioningStyle = 'Automatic';
+          if (config.ios?.appleTeamId) {
+            project.attributes.TargetAttributes[targetId].DevelopmentTeam = config.ios.appleTeamId;
+          }
+        }
       }
     }
 
